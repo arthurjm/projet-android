2f1473d5860e217131ed95a791242bdfbd5679e2
Mondon Arthur <arthur.mondon@gmail.com>
2019-03-01 11:28:09 +0100

    colorize.rs et keepHue.rs débuggés

--- app/src/main/rs/colorize.rs
+++ app/src/main/rs/colorize.rs
@@ -4,66 +4,68 @@
 int hue;
 
 uchar4 RS_KERNEL colorize ( uchar4 in ) {
-  uchar4 out = in;
+	uchar4 out = in;
 
-  float maxRGB = max(in.r, max(in.g, in.b));
-  float minRGB = min(min(in.r, in.g), in.b);
-  maxRGB = maxRGB/255;
-  minRGB = minRGB/255;
-  float delta = maxRGB - minRGB;
+	float r = in.r / 255.0;
+	float g = in.g / 255.0;
+	float b = in.b / 255.0;
 
-  float s, v;
-  // s
-  if (maxRGB == 0) {
-    s = 0;
-  }
-  else {
-    s = 1 - minRGB / maxRGB;
-  }
+	float maxRGB = max(r, max(g, b));
+	float minRGB = min(min(r, g), b);
+	float delta = maxRGB - minRGB;
 
-  //v
-  v = maxRGB;
+	float s, v;
+	// s
+	if (maxRGB == 0) {
+		s = 0;
+	}
+	else {
+		s = (maxRGB - minRGB) / maxRGB;
+	}
 
-  int h2;
-  float f, l, m, n;
-  float test = hue/60;
-  h2 = fmod(test, 6);
+	//v
+	v = maxRGB;
 
-  f = (hue/60) - h2;
-  l = v * (1 - s);
-  m = v * (1 - f * s);
-  n = v * (1 - (1 - f) * s);
+	int h2;
+	float f, l, m, n;
+	float h = hue/60.0;
+	h2 = fmod(h, 6);
 
-  if (h2 == 0) {
-    out.r = v * 255;
-    out.g = n * 255;
-    out.b = l * 255;
-  }
-  if (h2 == 1) {
-    out.r = m * 255;
-    out.g = v * 255;
-    out.b = l * 255;
-  }
-  if (h2 == 2) {
-    out.r = l * 255;
-    out.g = v * 255;
-    out.b = n * 255;
-  }
-  if (h2 == 3) {
-    out.r = l * 255;
-    out.g = m * 255;
-    out.b = v * 255;
-  }
-  if (h2 == 4) {
-    out.r = n * 255;
-    out.g = l * 255;
-    out.b = v * 255;
-  }
-  if (h2 == 5) {
-    out.r = v * 255;
-    out.g = l * 255;
-    out.b = m * 255;
-  }
+	f = h - h2;
+	l = v * (1 - s);
+	m = v * (1 - f * s);
+	n = v * (1 - (1 - f) * s);
 
-  return out;
+	if (h2 == 0) {
+		out.r = v * 255;
+		out.g = n * 255;
+		out.b = l * 255;
+	}
+	if (h2 == 1) {
+		out.r = m * 255;
+		out.g = v * 255;
+		out.b = l * 255;
+	}
+	if (h2 == 2) {
+		out.r = l * 255;
+		out.g = v * 255;
+		out.b = n * 255;
+	}
+	if (h2 == 3) {
+		out.r = l * 255;
+		out.g = m * 255;
+		out.b = v * 255;
+	}
+	if (h2 == 4) {
+		out.r = n * 255;
+		out.g = l * 255;
+		out.b = v * 255;
+	}
+	if (h2 == 5) {
+		out.r = v * 255;
+		out.g = l * 255;
+		out.b = m * 255;
+	}
+
+	return out;
 }
