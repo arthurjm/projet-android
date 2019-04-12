f1edc7b2a2896a2d56581e525bf280699825e763
mduban <mathieu.duban@etu-u.bordeaux.fr>
2019-04-12 20:58:44 +0200

    nouvelle façon de sauvegarder

--- app/src/main/java/com/package1/affichage/PhotoEditing.java
+++ app/src/main/java/com/package1/affichage/PhotoEditing.java
@@ -1,9 +1,13 @@
 package com.package1.affichage;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -33,7 +37,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
+import static android.content.ContentUris.*;
 import static com.package1.MainActivity.image;
 import static com.package1.MainActivity.imageEditing;
 import static com.package1.MainActivity.imageEditingCopy;
@@ -41,8 +47,8 @@ import static com.package1.MainActivity.imgView;
 
 /**
  * @author Mathieu
- *         In this class, we can fin different function like share an image, or save it
- *         This class coressponding to the layout apply_filter
+ * In this class, we can fin different function like share an image, or save it
+ * This class coressponding to the layout apply_filter
  */
 public class PhotoEditing extends AppCompatActivity {
 
@@ -185,7 +191,7 @@ public class PhotoEditing extends AppCompatActivity {
                 menuRecyclerView.setVisibility(View.VISIBLE);
                 filterRecyclerView.setVisibility(View.GONE);
                 back.setVisibility(View.GONE);
-                imageEditingCopy= imageEditing.copy(Bitmap.Config.ARGB_8888, true);
+                imageEditingCopy = imageEditing.copy(Bitmap.Config.ARGB_8888, true);
                 reset(imageEditing);
             }
         });
@@ -242,7 +248,11 @@ public class PhotoEditing extends AppCompatActivity {
 
     }
 
-    // opening image in default image viewer app
+    /**
+     * Open an image in default image viewer app
+     *
+     * @param path the path of the image
+     */
     private void openImage(String path) {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
@@ -257,34 +267,121 @@ public class PhotoEditing extends AppCompatActivity {
      */
     private void saveImageToGallery(Bitmap bmp) {
         File dir = new File(Environment.getExternalStorageDirectory(), "Projet");
-        if(dir.exists()){
+        if (dir.exists()) {
             dir.mkdirs();
         }
-        final String fileName = System.currentTimeMillis() + "";
-        File file = new File(dir, fileName);
+
+        final String path = insertImage(getContentResolver(), bmp, System.currentTimeMillis() + "_profile.jpg", null);
+        ConstraintLayout test = findViewById(R.id.applyFilter);
+        Snackbar snackbar = Snackbar.make(test, "Image sauvegardée dans la galerie !", Snackbar.LENGTH_LONG).setAction("OPEN", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openImage(path);
+            }
+        });
+        snackbar.show();
+
+    }
+
+    /**
+     * Storing image to device gallery
+     *
+     * @param cr the layout
+     * @param source the bitmap that we want to save
+     * @param title the name of the image
+     * @param description the description
+     * @return the path of this bitmap
+     */
+    public String insertImage(ContentResolver cr, Bitmap source, String title, String description) {
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, title);
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, title);
+        values.put(MediaStore.Images.Media.DESCRIPTION, description);
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        // Add the date meta data to ensure the image is added at the front of the gallery
+        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+
+        Uri url = null;
+        String stringUrl = null;    /* value to be returned */
 
         try {
-            FileOutputStream out = new FileOutputStream(file);
-            bmp.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            out.flush();
-            out.close();
-            Toast.makeText(context, "Image sauvegardée", Toast.LENGTH_SHORT).show();
-            ConstraintLayout test = findViewById(R.id.applyFilter);
-            Snackbar snackbar = Snackbar.make(test, "Image saved to gallery!", Snackbar.LENGTH_LONG).setAction("OPEN", new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    openImage(fileName);
+            url = cr.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+            if (source != null) {
+                assert url != null;
+                try (OutputStream imageOut = cr.openOutputStream(url)) {
+                    source.compress(Bitmap.CompressFormat.JPEG, 50, imageOut);
                 }
-            });
-            snackbar.show();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+                long id = parseId(url);
+                // Wait until MINI_KIND thumbnail is generated.
+                Bitmap miniThumb = MediaStore.Images.Thumbnails.getThumbnail(cr, id, MediaStore.Images.Thumbnails.MINI_KIND, null);
+                // This is for backward compatibility.
+                storeThumbnail(cr, miniThumb, id, 50F, 50F, MediaStore.Images.Thumbnails.MICRO_KIND);
+            } else {
+                assert url != null;
+                cr.delete(url, null, null);
+                url = null;
+            }
+        } catch (Exception e) {
+            if (url != null) {
+                cr.delete(url, null, null);
+                url = null;
+            }
         }
-        //add file to gallery
+
+        if (url != null) {
+            stringUrl = url.toString();
+        }
+
+        return stringUrl;
+    }
+
+
+    /**
+     * A copy of the Android internals StoreThumbnail method, it used with the insertImage to
+     * populate the android.provider.MediaStore.Images.Media#insertImage with all the correct
+     * meta data. The StoreThumbnail method is private so it must be duplicated here.
+     *
+     * @see android.provider.MediaStore.Images.Media (StoreThumbnail private method)
+     */
+    private Bitmap storeThumbnail(ContentResolver cr, Bitmap source, long id, float width, float height, int kind) {
+
+        // create the matrix to scale it
+        Matrix matrix = new Matrix();
+
+        float scaleX = width / source.getWidth();
+        float scaleY = height / source.getHeight();
+
+        matrix.setScale(scaleX, scaleY);
+
+        Bitmap thumb = Bitmap.createBitmap(source, 0, 0,
+                source.getWidth(),
+                source.getHeight(), matrix,
+                true
+        );
+
+        ContentValues values = new ContentValues(4);
+        values.put(MediaStore.Images.Thumbnails.KIND, kind);
+        values.put(MediaStore.Images.Thumbnails.IMAGE_ID, (int) id);
+        values.put(MediaStore.Images.Thumbnails.HEIGHT, thumb.getHeight());
+        values.put(MediaStore.Images.Thumbnails.WIDTH, thumb.getWidth());
+
+        Uri url = cr.insert(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, values);
+
         try {
-            MediaStore.Images.Media.insertImage(getContentResolver(), file.getAbsolutePath(), fileName, null);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            assert url != null;
+            OutputStream thumbOut = cr.openOutputStream(url);
+            thumb.compress(Bitmap.CompressFormat.JPEG, 100, thumbOut);
+            assert thumbOut != null;
+            thumbOut.close();
+            return thumb;
+        } catch (FileNotFoundException ex) {
+            return null;
+        } catch (IOException ex) {
+            return null;
         }
     }
 
